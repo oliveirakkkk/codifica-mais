3e32613d1d94d6d1231fb55864f1691be981cefe
Vinicius Oliveira <vinygiacomin@gmail.com>
2024-06-19 20:09:55 -0300

    Adicionando novo site

---
+++ PHP-ARRAYS/php-b8.php
@@ -0,0 +1,19 @@
+<?php
+
+$itensComprados = ["Carne", "Coca-cola", "Kaiser", "Pão de alho", "Queijo"];
+
+$preçoItensComprados ["Carne"] = 60;
+$preçoItensComprados ["Coca-cola"] = 20;
+$preçoItensComprados ["Kaiser"] = 30;
+$preçoItensComprados ["Pão de alho"] = 30;
+$preçoItensComprados ["queijo"] = 40;
+
+echo "Digite a quantidade de participantes do churrasco";
+
+$quantidadeParticipantes = trim(fgets(STDIN));
+
+function ValorQueCadaPessoaDevePagar ($preçoItensComprados, $quantidadeParticipantes) {
+
+    
+}
+
