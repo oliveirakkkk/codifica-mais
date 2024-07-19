e12b51f89cb0a81895d03d2ba09a35abb67fd84d
Vinicius Oliveira <vinygiacomin@gmail.com>
2024-07-19 12:41:28 -0300

    subindo exercicio de PHP POO

--- PHP-POO/EXERCICIO 1/execute.php
+++ PHP-POO/EXERCICIO 1/execute.php
@@ -2,4 +2,10 @@
 
 require_once "./Carro.php";
 
-$meuCarro = new Carro ();
+$meuCarro = new Carro ('nissan', 'Kicks',2018,'Amarelo');
+
+echo $carro -> getMarca(). PHP_EOL;
+echo $carro -> getModelo(). PHP_EOL;
+echo $carro -> getAno(). PHP_EOL;
+echo $carro -> acelerar(). PHP_EOL;
+echo $carro -> frear(). PHP_EOL;
