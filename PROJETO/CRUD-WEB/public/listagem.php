5d664f14c66a85e08ba043369ebadd53ab817f40
Vinícius Oliveira <vinygiacomin@gmail.com>
2024-08-15 20:57:25 -0300

    subindo parte da atividade 15/08 html

---
+++ PROJETO/CRUD-WEB/public/listagem.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/listagem.css">
+    <title>Listagem de produtos</title>
+</head>
+<body>
+
+    <div class="conteudoPrincipal">
+
+    <div class="novoItem"> </div>
+    
+    <div class="quadradoUm"> </div>
+
+    <div class="quadradoDois"> </div>
+    
+    <div class="quadradoTres"> </div>
+
+    </div>
+
+</body>
+</html>
